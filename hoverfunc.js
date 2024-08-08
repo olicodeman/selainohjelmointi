1f0ae7382ad3514c73852a35d2ac0a39e0898d92
Oliver McBeth <s3mcol00@students.osao.fi>
2024-08-08 09:08:54 +0300

    Contact details page updated

--- hoverfunc.js
+++ hoverfunc.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-    $("li").hover(function(){ // Homepage hover effect
+    $("li").hover(function(){ // Hover effect for picture gallery
         $(this).css("background-color", "darkslategray")
         $(this).css("border-radius", "15px")
     },
