ff13fd4866a08dc917536a77c5a5c550dc36e629
Oliver McBeth <s3mcol00@students.osao.fi>
2024-08-08 10:24:27 +0300

    updates

--- hoverfunc.js
+++ hoverfunc.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-    $("li").hover(function(){ // Hover effect for picture gallery
+    $("li").hover(function(){ // Hover effect for navbar
         $(this).css("background-color", "darkslategray")
         $(this).css("border-radius", "15px")
     },
