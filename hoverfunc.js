f422674944b7a1a436671066d1081437beb9881e
Oliver McBeth <s3mcol00@students.osao.fi>
2024-08-12 12:35:57 +0300

    Navigation bar changes

--- hoverfunc.js
+++ hoverfunc.js
@@ -3,10 +3,14 @@ $(document).ready(function(){
     $("li").hover(function(){ // Hover effect for navbar
         $(this).css("background-color", "darkslategray")
         $(this).css("border-radius", "15px")
+        $(this).find(".nav-link").css("color", "white")
+
+        
     },
 
     function(){
         $(this).css("background-color", "white")
+        $(this).find(".nav-link").css("color", "black")
     })
 
 });
