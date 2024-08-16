d1a8b54054d0848d98cf9f4de7398509787df1f2
Oliver McBeth <s3mcol00@students.osao.fi>
2024-08-16 13:09:24 +0300

    Page almost done

--- hoverfunc.js
+++ hoverfunc.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){ // Hover effect for navbar
 
     $("li").hover(function(){ 
-        $(this).css("background-color", "darkslategray")
+        $(this).css("background-color", "darkslategray") // changes backround color, border radius and font color when hovered over
         $(this).css("border-radius", "15px")
         $(this).find(".nav-link").css("color", "white")
 
         
     },
 
-    function(){
+    function(){ // Brings css styles back to normal when there is no more hovering
         $(this).css("background-color", "white")
         $(this).find(".nav-link").css("color", "black")
     })
